154d8c694780afde71e8d7a2b2b0197122dde17e
nilanjan-sikdar <nilanjansikdar66@gmail.com>
2026-08-27 15:57:46 +0530

    bug fixes

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,11 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import connectDB from "./config/db.js";
-
-dotenv.config();
+import authRoutes from "./routes/auth.routes.js";
 
 const app=express();
 const port=process.env.PORT || 8000;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello from auth");
